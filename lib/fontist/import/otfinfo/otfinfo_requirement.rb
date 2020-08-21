142ac96dd2192392905e40bb41d53cc751283ad8
Alexey Morozov <alexey.morozov.is@gmail.com>
2020-08-21 22:45:37 +0300

    Tell if there no otfinfo installed

---
+++ lib/fontist/import/otfinfo/otfinfo_requirement.rb
@@ -0,0 +1,20 @@
+module Fontist
+  module Import
+    module Otfinfo
+      class OtfinfoRequirement
+        def initialize
+          otfinfo_path = `which otfinfo`
+          if otfinfo_path.empty?
+            abort "otfinfo is not available. (Or is PATH not setup properly?)"\
+              " You must install otfinfo."\
+              " On macOS it can be installed via `brew install lcdf-typetools`."
+          end
+        end
+
+        def call(path)
+          `otfinfo --info '#{path}'`
+        end
+      end
+    end
+  end
+end
