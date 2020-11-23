d45dff8724c85e9f61ebe9dc5d23ba6095b796be
Alexey Morozov <alexey.morozov.is@gmail.com>
2020-11-23 17:16:02 +0300

    Subdir detection

--- lib/fontist/utils/zip_extractor.rb
+++ lib/fontist/utils/zip_extractor.rb
@@ -4,24 +4,21 @@ require "pathname"
 module Fontist
   module Utils
     module ZipExtractor
-      # fonts_sub_dir is unused now, but formulas have this option
-      # rubocop:disable Lint/UnusedMethodArgument
-      def zip_extract(resource, download: true, fonts_sub_dir: "")
+      def zip_extract(resource, download: true, fonts_sub_dir: nil)
         zip_file = download_file(resource) if download
         zip_file ||= resource.urls.first
 
         Fontist.ui.say(%(Installing font "#{key}".))
-        fonts_paths = unzip_fonts(zip_file)
+        fonts_paths = unzip_fonts(zip_file, fonts_sub_dir)
         block_given? ? yield(fonts_paths) : fonts_paths
       end
-      # rubocop:enable Lint/UnusedMethodArgument
 
       alias_method :unzip, :zip_extract
 
       private
 
       # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
-      def unzip_fonts(file)
+      def unzip_fonts(file, subdir)
         Zip.on_exists_proc = true
 
         Array.new.tap do |fonts|
@@ -29,7 +26,7 @@ module Fontist
             zip_file.each do |entry|
               if entry.name
                 filename = Pathname.new(entry.name).basename.to_s
-                if font_file?(filename)
+                if font_directory?(entry.name, subdir) && font_file?(filename)
                   target_filename = target_filename(filename)
                   font_path = fonts_path.join(target_filename)
                   fonts.push(font_path.to_s)
@@ -42,6 +39,14 @@ module Fontist
         end
       end
       # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
+
+      def font_directory?(path, subdir)
+        return true unless subdir
+
+        dirname = File.dirname(path)
+        normalized_pattern = subdir.chomp("/")
+        File.fnmatch?(normalized_pattern, dirname)
+      end
     end
   end
 end
