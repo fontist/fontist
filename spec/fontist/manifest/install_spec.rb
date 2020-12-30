5632170631c36ab24dface97f3e8a3a0db329a8b
Alexey Morozov <alexey.morozov.is@gmail.com>
2020-12-30 19:58:19 +0300

    Handle nil input to avoid unknown fails in tests

--- spec/fontist/manifest/install_spec.rb
+++ spec/fontist/manifest/install_spec.rb
@@ -3,13 +3,15 @@ require "spec_helper"
 RSpec.describe Fontist::Manifest::Install do
   describe ".from_hash" do
     let(:command) { described_class.from_hash(manifest) }
-    let(:manifest) { {} }
+    let(:manifest) { { "Andale Mono" => "Regular" } }
 
     context "confirmation option passed" do
       let(:command) { described_class.from_hash(manifest, confirmation: "yes") }
 
       it "accepts it with no error" do
-        expect { command }.not_to raise_error
+        no_fonts do
+          expect { command }.not_to raise_error
+        end
       end
     end
 
@@ -26,7 +28,16 @@ RSpec.describe Fontist::Manifest::Install do
     context "requires license confirmation and no flag passed" do
       before { stub_license_agreement_prompt_with("no") }
 
-      let(:manifest) { { "Andale Mono" => "Regular" } }
+      it "raises licensing error" do
+        no_fonts do
+          expect { command }.to raise_error Fontist::Errors::LicensingError
+        end
+      end
+    end
+
+    context "confirmation option passed as no and nil input is returned" do
+      let(:command) { described_class.from_hash(manifest, confirmation: "no") }
+      before { stub_license_agreement_prompt_with(nil) }
 
       it "raises licensing error" do
         no_fonts do
