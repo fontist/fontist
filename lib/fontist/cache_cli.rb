7db45f8e1da78d43b6ae2a7d4d727ef74db095f8
Alexande B <abobrikovich@gmail.com>
2023-03-06 21:14:36 +0100

    #323 fix cache clear on dir remove

--- lib/fontist/cache_cli.rb
+++ lib/fontist/cache_cli.rb
@@ -2,13 +2,27 @@ module Fontist
   class CacheCLI < Thor
     include CLI::ClassOptions
 
-    desc "clear", "Clear fontist download cache"
+    desc "clear", "Clear fontist cache"
     def clear
       handle_class_options(options)
       dir = Fontist.downloads_path
-      dir.each_child(&:delete) if dir.exist?
-      Fontist.ui.success("Download cache has been successfully removed.")
+      dir.each_child(&:rmtree) if dir.exist?
+      clear_indexes
+      Fontist.ui.success("Cache has been successfully removed.")
       CLI::STATUS_SUCCESS
+    end
+
+    private
+
+    def clear_indexes
+      delete_file_with_lock(Fontist.system_index_path)
+      delete_file_with_lock(Fontist.system_preferred_family_index_path)
+    end
+
+    def delete_file_with_lock(path)
+      path.delete if path.exist?
+      lock_path = Pathname.new Fontist::Utils::Cache.lock_path(path)
+      lock_path.delete if lock_path.exist?
     end
   end
 end
--- spec/fontist/cache_cli_spec.rb
+++ spec/fontist/cache_cli_spec.rb
@@ -4,16 +4,41 @@ require "fontist/cli"
 RSpec.describe Fontist::CacheCLI do
   describe "#clear" do
     let(:downloads_path) { Pathname.new "/fontist/download" }
+    let(:system_index) { Pathname.new "/fontist/system_index" }
+    let(:system_index2) { Pathname.new "/fontist/system_index2" }
     let(:command) { described_class.start(["clear"]) }
     let(:status) { command }
 
-    it "content deleted" do
+    before do
+      expect(Fontist).to receive(:downloads_path).and_return(downloads_path)
+      expect(Fontist).to receive(:system_index_path)
+        .and_return(system_index)
+        .at_least(:once)
+      expect(Fontist).to receive(:system_preferred_family_index_path)
+        .and_return(system_index2)
+        .at_least(:once)
+    end
+
+    it "download deleted" do
       cache_file = double("download cache file")
 
-      expect(cache_file).to receive(:delete).once
+      expect(cache_file).to receive(:rmtree).once
       expect(downloads_path).to receive(:exist?).and_return(true)
       expect(downloads_path).to receive(:each_child) { |&b| b.call(cache_file) }
-      expect(Fontist).to receive(:downloads_path).and_return(downloads_path)
+
+      command
+
+      expect(status).to be Fontist::CLI::STATUS_SUCCESS
+    end
+
+    it "indexes deleted" do
+      expect(system_index).to receive(:exist?).and_return(true)
+      expect(system_index2).to receive(:exist?).and_return(true)
+
+      expect(Fontist.system_index_path).to receive(:delete).once
+      expect(
+        Fontist.system_preferred_family_index_path,
+      ).to receive(:delete).once
 
       command
 
@@ -24,7 +49,7 @@ RSpec.describe Fontist::CacheCLI do
       allow(downloads_path).to receive(:exist?).and_return(false)
       allow(Fontist).to receive(:downloads_path).and_return(downloads_path).once
       expect(Fontist.ui).to receive(:success)
-        .with("Download cache has been successfully removed.")
+        .with("Cache has been successfully removed.")
 
       command
 
