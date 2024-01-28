61705e32ccec05bb3571fe35a61b2e82891d763b
jcbhmr <jcbhmr@outlook.com>
2024-01-27 19:30:19 -0600

    format

--- docs/.vitepress/theme/components/HomeContent.vue
+++ docs/.vitepress/theme/components/HomeContent.vue
@@ -1,27 +1,27 @@
 <template>
-    <div class="vp-doc container">
-        <slot />
-    </div>
+  <div class="vp-doc container">
+    <slot />
+  </div>
 </template>
 
 <style scoped>
 .container {
-margin: auto;
-width: 100%;
-max-width: 1280px;
-padding: 0 24px;
+  margin: auto;
+  width: 100%;
+  max-width: 1280px;
+  padding: 0 24px;
 }
 
 @media (min-width: 640px) {
-.container {
+  .container {
     padding: 0 48px;
-}
+  }
 }
 
 @media (min-width: 960px) {
-.container {
+  .container {
     width: 100%;
     padding: 0 64px;
-}
+  }
 }
 </style>
