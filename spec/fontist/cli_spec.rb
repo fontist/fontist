208ab62931d238fb0d9c2e5c505152c84124fd37
suleman-uzair <suleman@workytical.com>
2026-01-26 18:39:50 +0500

    fixing windows CI

--- spec/fontist/cli_spec.rb
+++ spec/fontist/cli_spec.rb
@@ -1108,7 +1108,7 @@ RSpec.describe Fontist::CLI do
 
       it "does not install font file" do
         if Fontist::Utils::System.user_os == :windows
-          allow(Fontist.ui).to receive(:ask).with(/TYPE 'yes' to continue, or press ENTER to cancel/).and_return(true)
+          allow(Fontist.ui).to receive(:ask).with(/TYPE 'yes' to continue, or press ENTER to cancel/).and_return("\n")
         end
         # Without --accept-all-licenses, licenses are declined by default
         command
@@ -1117,7 +1117,7 @@ RSpec.describe Fontist::CLI do
 
       it "tells that license should be confirmed in order for font to be installed" do
         if Fontist::Utils::System.user_os == :windows
-          allow(Fontist.ui).to receive(:ask).with(/TYPE 'yes' to continue, or press ENTER to cancel/).and_return(true)
+          allow(Fontist.ui).to receive(:ask).with(/TYPE 'yes' to continue, or press ENTER to cancel/).and_return("\n")
         end
 
         expect(Fontist.ui).to receive(:error).with("Fontist will not download these fonts unless you accept the terms.")
